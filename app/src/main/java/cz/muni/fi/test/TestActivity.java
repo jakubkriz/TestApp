b80ad2dc9205c8eb01507741d1b88624900bf209
Jakub Kriz <jakubkriz@users.noreply.github.com>
2015-05-18 08:23:29 +0200

    initial commit

---
+++ app/src/main/java/cz/muni/fi/test/TestActivity.java
@@ -0,0 +1,87 @@
+package cz.muni.fi.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        Button buttonRequest = (Button) findViewById(R.id.button_request);
+
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                PackageManager pm = getPackageManager();
+//                Intent intent = pm.getLaunchIntentForPackage("com.the.other.app");
+                Intent intent = new Intent();
+                intent.setAction("cz.muni.fi.digitrecognition.GAUGE_RECOGNITION");
+                intent.setClassName("cz.muni.fi.digitrecognition","cz.muni.fi.digitrecognition.CaptureActivity");
+
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 0);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Activity not found.", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null) {
+         int num1 = data.getIntExtra("GAUGE", 0);
+         if (num1 != 0) {
+             TextView dataView = (TextView) findViewById(R.id.dataView);
+             dataView.setText(Integer.toString(num1));
+         }
+        } else {
+            Toast toast = Toast.makeText(this, "No data.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
